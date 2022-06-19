3220b0ba880b4ce579b9f77b7417affca5f22b90
Rahul Roy <devrahul2508@gmail.com>
2022-06-19 21:02:08 +0530

    Commits

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ let interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
 const i = 0.3;
-i = 1;
+//i = 1;
 console.log(i);
 console.log("Test 2")
+let name1 = 'Rahul'; //String Literal
+let age = 30; //Number Literal
+let isApproved  = true; //Boolean Literal
+let firsName1 = undefined;
+let lastName1 = null;
