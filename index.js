6ebcf8f274601653846c4a74f66db4a694f09b91
Rahul Roy <devrahul2508@gmail.com>
2022-06-19 12:03:12 +0530

    Init Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let name = 'Rahul';
+console.log(name);
+
+let firstName = 'Rahul';
+let lastName = 'Roy';
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+const i = 0.3;
+i = 1;
+console.log(i);
