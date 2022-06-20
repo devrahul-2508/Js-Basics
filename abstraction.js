641583b1fed366e7e200cccf6e91952378d08795
Rahul Roy <devrahul2508@gmail.com>
2022-06-20 17:59:12 +0530

    Abstraction

---
+++ abstraction.js
@@ -0,0 +1,17 @@
+function Circle1(radius){
+    this.radius = radius;
+
+    let defaultLocation = { x:0 , y:0};
+
+    let computeOptimumLocation = function(factor){
+        //..
+    }
+
+    this.draw = function(){
+        
+        computeOptimumLocation();
+        console.log('draw');
+    };
+}
+const a = new Circle1(1);
+
